14233ded803428d0add869ce26d55d51e0978f28
Devansh-Balani8 <100129107+Devansh-Balani8@users.noreply.github.com>
2023-04-06 17:44:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,6 +19,16 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  video=createCapture(VIDEO);
+  video.size(700, 600);
+
+  poseNet=ml5.poseNet('video', modelLoaded);
+  poseNet.on('poses', gotPoses);
+}
+
+function moelLoaded(){
+  console.log('Model Loaded!');
 }
 
 
